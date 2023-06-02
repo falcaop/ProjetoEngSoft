9153290f7b9f86e3cadd541b1ffad04a17e7fe8f
falcaop <86216267+falcaop@users.noreply.github.com>
2023-06-02 08:19:36 -0300

    feat(ui): foundations and initial version of home

---
+++ src/components/FacilityCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+    sigla: String,
+    nome: String,
+    descricao: String,
+});
+</script>
+
+<template>
+    <div>
+        <h1>{{ sigla }}</h1>
+        <h4>{{ nome }}</h4>
+        <p>{{ descricao }}</p>
+    </div>
+</template>
+
+<style scoped>
+div{
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+    border-bottom-color: #99B6D0;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import FacilityCard from '../components/FacilityCard.vue';
+const facilities = [
+    {
+        sigla: 'UBS',
+        nome: 'Unidade Básica de Atendimento',
+        descricao: 'são locais onde os usuários do SUS podem receber atendimento médico para diagnóstico e tratamento de cerca de 80% dos problemas de saúde. É nessas unidades que a população tem acesso a medicamentos gratuitos e vacinas, etc.',
+    },
+    {
+        sigla: 'UBS',
+        nome: 'Unidade Básica de Atendimento',
+        descricao: 'são locais onde os usuários do SUS podem receber atendimento médico para diagnóstico e tratamento de cerca de 80% dos problemas de saúde. É nessas unidades que a população tem acesso a medicamentos gratuitos e vacinas, etc.',
+    },
+    {
+        sigla: 'UBS',
+        nome: 'Unidade Básica de Atendimento',
+        descricao: 'são locais onde os usuários do SUS podem receber atendimento médico para diagnóstico e tratamento de cerca de 80% dos problemas de saúde. É nessas unidades que a população tem acesso a medicamentos gratuitos e vacinas, etc.',
+    },
+    {
+        sigla: 'UBS',
+        nome: 'Unidade Básica de Atendimento',
+        descricao: 'são locais onde os usuários do SUS podem receber atendimento médico para diagnóstico e tratamento de cerca de 80% dos problemas de saúde. É nessas unidades que a população tem acesso a medicamentos gratuitos e vacinas, etc.',
+    },
+];
+</script>
+
+<template>
+    <section>
+        <FacilityCard v-for="(facility, i) in facilities" :key="i" v-bind="facility"/>
+    </section>
+</template>
